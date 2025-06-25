8cea2564e54c2db19e4cd6f5ed5bb2571a09a59d
Lucas <lucas.duartebarbosa@outlook.com>
2025-06-25 09:18:29 -0300

    projeto iniciado e configurado

---
+++ api-boleto-kafka/src/main/java/br/com/duarte/api_boleto_kafka/ApiBoletoKafkaApplication.java
@@ -0,0 +1,13 @@
+package br.com.duarte.api_boleto_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBoletoKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBoletoKafkaApplication.class, args);
+	}
+
+}
---
+++ api-boleto-kafka/src/test/java/br/com/duarte/api_boleto_kafka/ApiBoletoKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.duarte.api_boleto_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBoletoKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
